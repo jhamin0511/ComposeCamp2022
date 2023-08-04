d81cd53ba536454580e26bf229fd2cb2f138c553
jeonghamin <jhamin0511@gmail.com>
2023-08-04 16:31:00 +0900

    feat(step10): ViewCompositionStrategy

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -22,6 +22,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -93,7 +94,11 @@ class PlantDetailFragment : Fragment() {
                     }
                 }
             )
-            composeView.setContent {
+            composeView.apply {
+                setViewCompositionStrategy(
+                    ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+                )
+            }.setContent {
                 MaterialTheme {
                     PlantDetailDescription(plantDetailViewModel)
                 }
